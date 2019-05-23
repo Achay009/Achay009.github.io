7bfeb70430de3348e3c9ac105a1ee51a1f76be28
Achay <ucheAchay3@gmail.com>
2019-05-23 22:05:50 +0100

    intial

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".loader").style.display = "block";
+    console.log("Api call has started");
+    var input = document.getElementById('inputBvn');
+    var bvn = input.value;
+
+    var url = "https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/" + bvn + "?seckey=FLWSECK-e6db11d1f8a6208de8cb2f94e293450e-X";
+
+    var fetchData = {
+        request: 'GET',
+        header: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    fetch(url, fetchData)
+        .then(res => res.json())
+        .then((response) => {
+            if (response.status == "success") {
+                console.log(response);
+                alert("Validation Succes " + response.data.first_name + " " + response.data.last_name);
+            } else {
+                alert("Cannot find BVN holder");
+            }
+            document.querySelector(".loader").style.display = "none";
+        }).catch(error => alert("Opps Something Occured try again"));
+
+
+   
+
+    console.log("Api call has finished");
+
+})
