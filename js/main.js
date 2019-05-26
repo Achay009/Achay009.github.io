4465f6d6991efb708e5693f2839f04e688542d1a
Achay009 <40715909+Achay009@users.noreply.github.com>
2019-05-26 17:51:35 +0100

    Update main.js
    commenting

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById("btn");
-
+//get the bvn input
 btn.addEventListener("click", function (e) {
     document.querySelector(".formSuccess").style.display = "none";
    document.querySelector(".formError").style.display = "none";
@@ -10,19 +10,22 @@ btn.addEventListener("click", function (e) {
     var input = document.getElementById('inputBvn');
     var bvn = input.value;
 
+    //construct the url
     var url = "https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/" + bvn + "?seckey=FLWSECK-e6db11d1f8a6208de8cb2f94e293450e-X";
-
+    
+    //create fetch data
     var fetchData = {
         request: 'GET',
         header: {
             'Content-Type': 'application/json'
         }
     }
-if(!isNaN(bvn)){
+    //validate bvn length and property
+if(!isNaN(bvn) || bvn.length != 11){
     fetch(url, fetchData)
     .then(res => res.json())
     .then((response) => {
-       
+       //get the response
         if (response.status == "success") {
             console.log(response);
             document.querySelector(".loader").style.display = "none";
@@ -54,7 +57,7 @@ console.log("Api call has finished");
 
     
 }else{
-    
+    //if it does not pass validation and property test
     document.querySelector(".loader").style.display = "none";
     document.querySelector(".formError").style.display = "block";
     document.querySelector(".formError").innerText = "Oops!! please Enter BVN number";
